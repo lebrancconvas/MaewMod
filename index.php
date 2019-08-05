b3c5a21cf5c01e5667f497da0d37ebec1004779a
lebrancconvas <lebranccconvas@gmail.com>
2019-08-06 01:05:13 +0700

    :tada: Website.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Maew Mod Story</title>
+    <style>
+        body {
+            border: 0;
+            margin: 0;
+        }
+
+        .butt {
+            background: green;
+            border: 20px;
+            border-radius: 10px;
+            box-shadow: 1px 1px 1px 1px rgba(0,0,0,0.75);
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello Maew</h1>
+    <div class="butt">
+        Press for President
+    </div>
+</body>
+</html>
